8c85cd65c0789c711321e95cdc225ccf6c473a97
emySchoettel <e.schoettel@ludus-academie.com>
2021-05-29 18:48:09 +0200

    turret added part 1

--- Source/ToonTanks/Pawns/PawnTank.h
+++ Source/ToonTanks/Pawns/PawnTank.h
@@ -23,8 +23,9 @@ class TOONTANKS_API APawnTank : public APanwBase
 
 		FVector MoveDirection; 
 		FQuat RotationDirection; 
-
+		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement", meta = (AllowPrivateAccess = "true" ))
 		float MoveSpeed = 100.0f; 
+		UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement", meta = (AllowPrivateAccess = "true" ))
 		float RotateSpeed = 100.0f;
 		
 		void CalculateMoveInput(float Value);
